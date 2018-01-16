e1ef38510c6a73c3e0c24f966c87ec4b7012975a
Jens Viebig <jsurf@gmx.de>
2018-01-16 22:32:51 +0100

    Raspberry emulator

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM debian:buster-slim
+RUN apt-get update \
+    && apt-get -q --no-install-recommends -y install \
+	      qemu-system-arm \
+		  wget \
+          ca-certificates \
+          wget \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+CMD /run.sh
+
+#RUN wget https://downloads.raspberrypi.org/raspbian_lite_latest -qO raspian.img \
+#    && mkdir -p /mnt/img
+RUN	mkdir -p /mnt/img
+ADD ./run.sh /run.sh
+#ADD 2017-11-29-raspbian-stretch-lite.img /tmp
+
+#sfdisk -d os.img | grep img2 | sed -r 's/.*start=\s*([0-9]+).*size.*/\1/'
+# $(($(sfdisk -d os.img | grep img2 | sed -r 's/.*start=\s*([0-9]+).*size.*/\1/') * 512))
+# qemu-system-arm -M raspi2
+# 	      sfdisk \	  
+#mount -v -o offset=48234496 -t ext4 /os.img /mnt/img
---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+OFFSET=$(($(sfdisk -d /os.img | grep img1 | sed -r 's/.*start=\s*([0-9]+).*size.*/\1/') * 512))
+#OFFSET=$(($(sfdisk -d /os.img | grep img2 | sed -r 's/.*start=\s*([0-9]+).*size.*/\1/') * 512))
+mount -v -o offset=$OFFSET -t vfat /os.img /mnt/img
+CMDLINE="dwc_otg.lpm_enable=0 console=serial0,115200 console=tty1 root=PARTUUID=37665771-02 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait quiet init=/usr/lib/raspi-config/init_resize.sh"
+#CMDLINE="rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 init=/bin/bash"
+#CMDLINE="earlyprintk loglevel=8 dwc_otg.lpm_enable=0 console=serial0,115200 console=tty1 root=PARTUUID=37665771-02 rootfstype=ext4 elevator=deadline init=/usr/lib/raspi-config/init_resize.sh"
+qemu-system-arm -M raspi2 -kernel /mnt/img/kernel7.img -vnc :1 -append "$CMDLINE" -drive file=/os.img,if=sd,format=raw -dtb /mnt/img/bcm2709-rpi-2-b.dtb -serial stdio
+
+#qemu-system-arm -M raspi2 -kernel boot/kernel7.img -drive format=raw,file=2017-11-29-raspbian-stretch-lite.img -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2" -dtb 2015-11-21-raspbian-boot/bcm2709-rpi-2-b.dtb -serial stdio
+#dwc_otg.lpm_enable=0 console=serial0,115200 console=tty1 root=PARTUUID=37665771-02 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait quiet init=/usr/lib/raspi-config/init_resize.sh
+#mv /mnt/img/etc/ld.so.preload /mnt/img/etc/ld.so.preload.disabled
+#sed --in-place=".org" "s,/dev/mmcblk0p1,/dev/sda1," /mnt/img/etc/fstab
+#sed --in-place=".org" "s,/dev/mmcblk0p2,/dev/sda2," /mnt/img/etc/fstab
