8b02860c6e3a41ec32b885115803e6a4a2203412
Jens Viebig <jsurf@gmx.de>
2018-01-16 23:00:40 +0100

    It is booting, keyboard not working

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 OFFSET=$(($(sfdisk -d /os.img | grep img1 | sed -r 's/.*start=\s*([0-9]+).*size.*/\1/') * 512))
 #OFFSET=$(($(sfdisk -d /os.img | grep img2 | sed -r 's/.*start=\s*([0-9]+).*size.*/\1/') * 512))
 mount -v -o offset=$OFFSET -t vfat /os.img /mnt/img
-CMDLINE="dwc_otg.lpm_enable=0 console=serial0,115200 console=tty1 root=PARTUUID=37665771-02 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait quiet init=/usr/lib/raspi-config/init_resize.sh"
+CMDLINE="earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=PARTUUID=37665771-02 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait"
 #CMDLINE="rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 init=/bin/bash"
 #CMDLINE="earlyprintk loglevel=8 dwc_otg.lpm_enable=0 console=serial0,115200 console=tty1 root=PARTUUID=37665771-02 rootfstype=ext4 elevator=deadline init=/usr/lib/raspi-config/init_resize.sh"
 qemu-system-arm -M raspi2 -kernel /mnt/img/kernel7.img -vnc :1 -append "$CMDLINE" -drive file=/os.img,if=sd,format=raw -dtb /mnt/img/bcm2709-rpi-2-b.dtb -serial stdio
